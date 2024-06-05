bdd68abf0f4dfadbb37b6f2cde45f08042a45e60
RuchikaKadam <kadamruchikafullstack.com>
2024-06-05 22:38:40 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {useState} from 'react';
+import {data} from './constant';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(amount < 0) amount = 1;
+    if(amount > 20) amount = 20;
+    setText(data.slice(0,amount));
+    return;
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <section className='section-center'>
+          <h3>Alternative for lorem ipsum!</h3>
+          <form className='lorem-form' onSubmit={handleSubmit}>
+            <input type="number" name="amount" id="amount" placeholder='Number of Paragraphs' value={count} onChange={(e)=>setCount(e.target.value)}/>
+            <button className='btn'>Generate Paragraphs</button>
+          </form>
+          <article>
+            {text.map((data,index) => (
+              <p key={index}>{data}</p>
+            ))}
+          </article>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
